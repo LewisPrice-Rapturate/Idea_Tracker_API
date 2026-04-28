f754982d196a9497f587e84a16041e858fe4f9b0
Rapturate <lewis.e.price@outlook.com>
2026-04-28 10:13:09 -0400

    Added full documentation website into server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.get('/health', (req, res) => res.status(200).json({ status: 'UP' }));
-app.get('/', (req, res) => res.status(200).send('API is running...\nVisit /api-docs for API documentation'));
+app.get('/', (req, res) => res.status(200).send('API is running...\nVisit https://idea-tracker-api.onrender.com/api-docs for API documentation'));
 if (process.env.NODE_ENV !== 'test') app.use(morgan('tiny'));
 
 let specs;
