d590a65c5e787c34da2417cc32193db206c6ee2f
Rapturate <lprice56@charlotte.edu>
2026-04-28 00:32:41 -0400

    Added a health check route to see if that helps.

--- src/server.js
+++ src/server.js
@@ -19,6 +19,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.get('/health', (req, res) => res.status(200).json({ status: 'UP' }));
+app.get('/', (req, res) => res.status(200).send('API is running...'));
 if (process.env.NODE_ENV !== 'test') app.use(morgan('tiny'));
 
 let specs;
